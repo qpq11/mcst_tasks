1b38f6230f92f9a217df85ee842d2ae48ed53782
qpq11 <vanag2040@gmail.com>
2025-03-07 22:13:03 +0300

    "rewrote" task 10
    rewrote parcer for logical expressions; debugging yet to do

--- task 10/mcst10.c
+++ task 10/mcst10.c
@@ -1,134 +1,118 @@
-# вариант- читать как в дифференциалке, спуском. потом взять значения переменных и вычислить ответ
-
-// #define Require(ch) \
-// if (*s == ch) s++; \
-// else SyntaxError();
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
-#include "recdesc.h"
+#include <ctype.h>
 
-const int maxLen = 1000;
+const int maxLen    = 1000;
+const int maxVarCnt =  100;
+const int nameLen   =   50;
 
-int main()
-{
-	FILE* fp = fopen("text.txt", "r");
-	
-	char* reads = (char*) calloc(sizeof(char), (maxLen + 1));
-	assert(reads != nullptr);
-	const int len = FileGet(reads, fp);
-	int res= GetG(reads);
-	printf("%d \n", res);
-	return 0;
+typedef struct {
+    char name[nameLen];
+    int value;
+} IntVar;
+
+IntVar vars[maxVarCnt];
+
+int GetN(const char* name);
+int GetOr(char** s);
+int GetAnd(char** s);
+int GetNot(char** s);
+void SkipSpaces(char** s);
+void SyntaxError();
+
+int main() {
+    FILE* fp = fopen("text.txt", "r");
+    if (!fp) {
+        printf("Text file opening error.");
+        return -1;
+    }
+
+	int varCnt = 0;
+    char* reads = (char*) calloc(maxLen + 1, sizeof(char));
+    if (!reads) {
+        printf("Memory allocation error.");
+        return -2;
+    }
+
+    fgets(reads, maxLen, fp);
+    fclose(fp);
+
+    char* s = reads;
+    SkipSpaces(&s);
+
+    int result = GetOr(&s);
+    printf("Result: %d\n", result);
+
+    free(reads);
+    return 0;
 }
 
-int GetN(char** s)
-{
-	int val = 0;
-	char* oldS = *s;
-	while ('0' <= **s && **s <= '9')
-	{
-		val = val*10 + (**s - '0');
-		*s += 1;
-	}
-	if (oldS == *s)
-	{
+int GetN(const char* name) {
+    for (int i = 0; i < varCnt; i++) {
+        if (strcmp(vars[i].name, name) == 0)
+            return vars[i].value;
+    }
+    printf("Enter value for %s (typically 0 or 1): ", name);
+    int val;
+    scanf("%d", &val);
+    vars[varCnt].value = val;
+    strcpy(vars[varCnt].name, name);
+    varCnt++;
+    return val;
+}
+
+int GetOr(char** s) {
+    int val = GetAnd(s);
+    while (strncmp(*s, "OR", 2) == 0) {
+        *s += 2;
+        int val2 = GetAnd(s);
+        val = val || val2;
+    }
+    return val;
+}
+
+int GetAnd(char** s) {
+    int val = GetNot(s);
+    SkipSpaces(s);
+    while (strncmp(*s, "AND", 3) == 0) {
+        *s += 3;
+        int val2 = GetNot(s);
+        val = val && val2;
+    }
+    return val;
+}
+
+int GetNot(char** s) {
+    SkipSpaces(s);
+    if (strncmp(*s, "NOT", 3) == 0) {
+        *s += 3;
+        return !GetNot(s);
+    }
+    
+    if (**s == '(') {
+        (*s)++;
+        int val = GetOr(s);
+        if (**s != ')') 
+			SyntaxError();
+        (*s)++;
+        return val;
+    }
+    
+    char varName[nameLen];
+    int i = 0;
+    while (isalpha(**s) && (i < (nameLen - 1))) {
+        varName[i++] = **s;
+        (*s)++;
+    }
+    varName[i] = '\0';
+
+    if (i == 0) 
 		SyntaxError();
-	}
-	//printf("%d\n", val);
-	return val;
+    return GetN(varName);
 }
 
-// printf("n %c\n", **s);
-	// char* oldS = *s;
-	
-	// if('A' <= **s && 'Z' >= **s)
-	// {
-		// Node* val = CreateNode(VAR, *s, NULL, NULL);
-		// *s += 1;
-		// SPACESKIP(s);
-		// printf("n %c\n", **s);
-		// return val;
-	// }
-
-int GetP(char** s)
-{
-	//puts("p");
-	if (**s == '(')
-	{
-		*s += 1;
-		int val = GetE(s);
-		//s++;
-		//printf("$%d\n", val);
-		Require(')');
-		return val;
-	}
-	int val = GetN(s);
-	return val;
-}
-
-int GetT(char** s)
-{
-	//puts("t");
-	int val = GetP(s);
-	while (**s == '*' || **s == '/')
-	{
-		char op = **s;
-		//printf("%c \n", *s);
-		*s += 1;
-		int val2 = GetP(s);
-		if (op == '*') val *= val2; //val = CreateNode(OP, &op_mul, val, val2);
-		else           val /= val2; //val = CreateNode(OP, &op_div, val, val2);
-	}
-	return val;
-}
-
-int GetE(char** s)
-{
-	//puts("e");
-	int val = GetT(s);
-	while (**s == 'A' || **s == '')
-	{
-		char op = **s;
-		*s += 1;
-		int val2 = GetT(s);
-		if (op == '+') val += val2; //val = CreateNode(OP, &op_add, val, val2);
-		else           val -= val2; //val = CreateNode(OP, &op_sub, val, val2);
-	}
-	//printf("%d\n", val);
-	return val;
-}
-
-int GetG(char* str)
-{
-	//puts("g");
-	char** s = (char**) &str;
-	//s = str;
-	int val = GetE(s);
-	Require('$');
-	return val;
-}
-
-int FileGet(char* reads, FILE *fp)
-{
-	
-	int len  = 0;
-	int ch   = 0;
-	
-	while((ch = fgetc(fp)) != EOF && (ch != '\n'))
-	{
-		*(reads + len) = ch;
-		//printf("%d  %c$\n", ch, ch);
-		len++;
-	}
-	printf("%d symbols in file\n", len);
-	return len;
-}
-
-void SyntaxError()
-{
-	printf("Syntax Error.\n");
-	assert(0 == 1);
+void SyntaxError() {
+    printf("Syntax Error\n");
+    exit(1);
 }
