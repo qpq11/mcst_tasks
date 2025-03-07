fb9cb851928557fbc695ba1d33bc19d2f5389800
qpq11 <vanag2040@gmail.com>
2025-03-07 15:12:04 +0300

    task 9 finished
    fixed code in task 9; added makefile and the comments.

--- task 9/mcst9.c
+++ task 9/mcst9.c
@@ -4,55 +4,62 @@
 int main()
 {
 	int arrSize = 0;
-	scanf("%d", &arrSize);
+	printf("Enter the amount of sorted elems:");
+	scanf("%d", &arrSize); //установить длину сортированного массива
 	
-	int* arr = (int*) calloc(arrSize, sizeof(int));
+	if (arrSize <= 0) //разрешить случаи, когда сортировать нечего
+	{
+		printf("[ 0 ]\n 0");
+		return 0;
+	}
+	
+	int* arr = (int*) calloc(arrSize, sizeof(int)); //собственно массив с элементами
     printf("Enter %d elems: ", arrSize);
     for (int i = 0; i < arrSize; i++)
         scanf("%d", &arr[i]);
 	
-	int* d = (int*) calloc(arrSize, sizeof(int));
-	
-	// int currSum  = 0;
-	// int maxSum   = 0;
+	int* d = (int*) calloc(arrSize, sizeof(int)); //массив максимальных длин последовательностей, начинающихся в
+	//каждом arr[i]
 	
 	int j = 0;
-	int maxLen = 0;
-	int maxStart=0;
+	int maxLen = 0; //длина искомой подпоследовательности
+	int maxStart=0; //индекс начала
 	while (j < arrSize)
 	{
 		d[j] = 1;
-		// currSum = arr[j];
 		int k;
 		for (k = j + 1; k < arrSize; k++)
 		{
-			if(arr[k] > arr[j])
+			if(arr[k] > arr[k-1]) //если последовательность возрастает
 			{
-				printf("%d %d %d\n", arr[k], arr[j], d[j]);
-				d[j]++;
-				// currSum += arr[k];
+				//printf("%d %d %d\n", arr[k], arr[j], d[j]);
+				d[j]++; //ищем ее дальше
 			}
-			else
+			else //конец подпоследовательности
 			{	
-				break;
+				break; //прекращаем рассматривать этот arr[j]
 			}
 		}
-		if(maxLen < d[j])
+		if(maxLen < d[j]) //если длина превысила максимальную, перезаписываем
 		{
 			maxLen = d[j];
 			maxStart = j;
 		}
 		
-		j=k;
+		j=k; //если нашли конец подпоследовательности с началом в arr[j], то в элементах arr вплоть до ее конца
+		//искать нам нечего
 	}
 	
 	int maxSum = 0;
-	puts("----");
-	for (int i = 0; i < maxLen; i++)
+	puts("----"); //конец поиска
+	printf("[ ");
+	for (int i = 0; i < maxLen; i++) //вычисляем сумму по нашим индексу начала и длине; параллельно выводим
+	//саму последовательность поэлементно
 	{
-		printf("%d\n", arr[maxStart + i]);
+		printf("%d ", arr[maxStart + i]);
 		maxSum += arr[maxStart + i];
 	}
+	printf("]\n");
 	printf("\n%d\n", maxSum);
 	free(arr);
 	free(d);
