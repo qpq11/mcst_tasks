be9253619b3695a54c121cbda30bd5aad06fc523
qpq11 <vanag2040@gmail.com>
2025-03-07 23:57:27 +0300

    task 6 done
    finished task 6 + comments

--- task 6/mcst6.c
+++ task 6/mcst6.c
@@ -4,27 +4,43 @@
 
 int main(int argc, char** argv)
 {
-	if (argc == 2)
+	if (argc != 2) //ровно 2 аргмента разрешены
 	{
 		printf("program only takes 2 arguments: executable file name and name of the file we give to stat.\n");
 		return -1;
 	}
 	
-	struct stat fileStats;
+	struct stat fileStats; //структура со статистикой о файле
 	
 	if (stat(argv[1], &fileStats) == -1)
 	{
 		printf("ERROR: stat returned -1.\n");
 		return -2;
 	}
-	
+	printf("INPUT FILE STATS\n\n");
+	printf("  NAME                   |  %s\n", argv[1]);
+	printf("-----------------------------------------------------------------\n");
+	//тип файла
 	 if (S_ISREG(fileStats.st_mode))
-        printf("  Type: Regular file\n");
+        printf("  TYPE                   |  Regular file\n");
     else if (S_ISDIR(fileStats.st_mode))
-        printf("  Type: Directory\n");
-
-    printf("  Size: %lld bytes\n", (long long)fileStats.st_size);
-    printf("  Permissions: %o\n", fileStats.st_mode & 0777);
+        printf("  TYPE                   |  Directory\n");
+    else if (S_ISFIFO(fileStats.st_mode))
+        printf("  TYPE                   |  FIFO (named pipe)\n");
+    else if (S_ISLNK(fileStats.st_mode))
+        printf("  TYPE                   |  Symbolic link\n");
+    else if (S_ISLNK(fileStats.st_mode))
+        printf("  TYPE                   |  Socket\n");
+	else if (S_ISCHR(fileStats.st_mode))
+        printf("  TYPE                   |  Character device\n");
+    else if (S_ISBLK(fileStats.st_mode))
+        printf("  TYPE                   |  Block device\n");
+    else
+		printf("  TYPE                   |  Unknown\n");
+	//размер, разрешения, время последнего изменения с начала Unix(?)
+    printf("  SIZE                   |  %lld bytes\n", (long long)fileStats.st_size);
+    printf("  PERMITS                |  %o\n", fileStats.st_mode & 0777);
+    printf("  LAST MODIFIED          |  %ld\n\n", (long)fileStats.st_mtime);
 	
 	return 0;
 }
